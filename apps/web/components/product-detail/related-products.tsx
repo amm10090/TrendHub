bd3f854564cc45f75770ad42632b32983a7f6b91
amm10090 <amm10090@163.com>
2025-04-21 00:46:03 +0800

    chore(pnpm): 更新依赖版本和添加补丁依赖
    在pnpm-lock.yaml中更新了@types/node的版本至22.13.17，并在pnpm-workspace.yaml中添加了@heroui/dom-animation的补丁依赖。同时，删除了admin应用中的tailwind配置文件以简化项目结构。此变更旨在确保依赖的最新性和项目的整洁性，无明显副作用。

--- apps/web/components/product-detail/related-products.tsx
+++ apps/web/components/product-detail/related-products.tsx
@@ -134,7 +134,7 @@ export function RelatedProducts({ products }: RelatedProductsProps) {
                   <h3 className="text-[11px] sm:text-sm font-semibold tracking-wide text-text-secondary-light dark:text-text-secondary-dark">
                     {product.brand}
                   </h3>
-                  <Link className="block flex flex-col gap-y-1" href={`/product/${product.id}`}>
+                  <Link className="block  flex-col gap-y-1" href={`/product/${product.id}`}>
                     <p className="text-[11px] sm:text-sm font-normal text-text-primary-light dark:text-text-primary-dark line-clamp-2 leading-relaxed">
                       {product.name}
                     </p>
